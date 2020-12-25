a6b867e09c421e18d7f4601575874d77be6ca4c2
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-12-25 23:56:57 +0530

    Update Bellmanford.c

--- Lab 3/Bellmanford.c
+++ Lab 3/Bellmanford.c
@@ -1,48 +1,53 @@
+/*
+Given a graph, each node A knows the shortest path to node Z and node A can determine its shortest path to Z by calculating the minimum cost.
+Now when packet flows through a path it incurs some cost to the network, find shortest paths from src to all nodes in the given graph using Bellman Ford Algorithm.
+The graph may contain negative weight edges.
+*/
 #include<stdio.h>
 struct node
 {
-    unsigned dist[20];
-    unsigned from[20];
+Â  Â  unsigned dist[20];
+Â  Â  unsigned from[20];
 }rt[10];
 int main()
 {
-    int costmat[20][20];
-    int nodes,i,j,k,count=0;
-    printf("\nEnter the number of nodes : ");
-    scanf("%d",&nodes);//Enter the nodes
-    printf("\nEnter the cost matrix :\n");
-    for(i=0;i<nodes;i++)
-    {
-        for(j=0;j<nodes;j++)
-        {
-            scanf("%d",&costmat[i][j]);
-            costmat[i][i]=0;
-            rt[i].dist[j]=costmat[i][j];
-            rt[i].from[j]=j;
-        }
-    }
-        do
-        {
-            count=0;
-            for(i=0;i<nodes;i++)
-            for(j=0;j<nodes;j++)
-            for(k=0;k<nodes;k++)
-                if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
-                {
-                    rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
-                    rt[i].from[j]=k;
-                    count++;
-                }
-        }while(count!=0);
-        for(i=0;i<nodes;i++)
-        {
-            printf("\n\n For router %d\n",i+1);
-            for(j=0;j<nodes;j++)
-            {
-                printf("\t\nnode %d via %d Distance %d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
-            }
-        }
-    printf("\n\n");
+Â  Â  int costmat[20][20];
+Â  Â  int nodes,i,j,k,count=0;
+Â  Â  printf("\nEnter the number of nodes : ");
+Â  Â  scanf("%d",&nodes);//Enter the nodes
+Â  Â  printf("\nEnter the cost matrix :\n");
+Â  Â  for(i=0;i<nodes;i++)
+Â  Â  {
+Â  Â  Â  Â  for(j=0;j<nodes;j++)
+Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  scanf("%d",&costmat[i][j]);
+Â  Â  Â  Â  Â  Â  costmat[i][i]=0;
+Â  Â  Â  Â  Â  Â  rt[i].dist[j]=costmat[i][j];
+Â  Â  Â  Â  Â  Â  rt[i].from[j]=j;
+Â  Â  Â  Â  }
+Â  Â  }
+Â  Â  Â  Â  do
+Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  count=0;
+Â  Â  Â  Â  Â  Â  for(i=0;i<nodes;i++)
+Â  Â  Â  Â  Â  Â  for(j=0;j<nodes;j++)
+Â  Â  Â  Â  Â  Â  for(k=0;k<nodes;k++)
+Â  Â  Â  Â  Â  Â  Â  Â  if(rt[i].dist[j]>costmat[i][k]+rt[k].dist[j])
+Â  Â  Â  Â  Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  Â  Â  Â  Â  rt[i].dist[j]=rt[i].dist[k]+rt[k].dist[j];
+Â  Â  Â  Â  Â  Â  Â  Â  Â  Â  rt[i].from[j]=k;
+Â  Â  Â  Â  Â  Â  Â  Â  Â  Â  count++;
+Â  Â  Â  Â  Â  Â  Â  Â  }
+Â  Â  Â  Â  }while(count!=0);
+Â  Â  Â  Â  for(i=0;i<nodes;i++)
+Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  printf("\n\n For router %d\n",i+1);
+Â  Â  Â  Â  Â  Â  for(j=0;j<nodes;j++)
+Â  Â  Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  Â  Â  printf("\t\nnode %d via %d Distance %d",j+1,rt[i].from[j]+1,rt[i].dist[j]);
+Â  Â  Â  Â  Â  Â  }
+Â  Â  Â  Â  }
+Â  Â  printf("\n\n");
 
 }
 
