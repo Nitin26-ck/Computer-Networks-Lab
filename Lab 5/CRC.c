c8d0f91a0345e94a06a8bbab3ba4acb9e7b61873
Nitin C Kallappa <nitinckallappa@gmail.com>
2020-12-28 17:18:18 +0530

    Update CRC.c

--- Lab 5/CRC.c
+++ Lab 5/CRC.c
@@ -1,3 +1,5 @@
+/*Write a program for implementing the error detection technique for data transfer in unreliable network code using CRC (16-bits) Technique.
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -7,113 +9,113 @@
 void divide(char agdtw[],char divs[],char remd[])
 
 {
-    int i,r,l,a,t;
-    r=strlen(divs);
-    t=strlen(agdtw)-r+1;
-    char divd[18],rem[18];
-    strncpy(divd,agdtw,r);
-    divd[r]='\0';
-    l=0;
-    memset(rem, 0,18);
-    while(l<t)
-    {
-        a=0;
-        memset(rem, 0,18);
-        if(divd[0]==divs[0])
-        {
-            for(i=1;i<r;i++)
-            {
-                if(divd[i]==divs[i])
-                    rem[a++]='0';
-                else
-                    rem[a++]='1';
-            }
-            rem[a]='\0';
-            strcpy(divd,rem);
-        }
-        else
-        {
-            strncpy(divd,&divd[1],strlen(divd)-1);
-            divd[r-1]='\0';
-        }
-        int o=strlen(divd);
-        divd[o]=agdtw[l+r];
-        divd[r]='\0';
-        l++;
-    }
+Â  Â  int i,r,l,a,t;
+Â  Â  r=strlen(divs);
+Â  Â  t=strlen(agdtw)-r+1;
+Â  Â  char divd[18],rem[18];
+Â  Â  strncpy(divd,agdtw,r);
+Â  Â  divd[r]='\0';
+Â  Â  l=0;
+Â  Â  memset(rem, 0,18);
+Â  Â  while(l<t)
+Â  Â  {
+Â  Â  Â  Â  a=0;
+Â  Â  Â  Â  memset(rem, 0,18);
+Â  Â  Â  Â  if(divd[0]==divs[0])
+Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  for(i=1;i<r;i++)
+Â  Â  Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  Â  Â  if(divd[i]==divs[i])
+Â  Â  Â  Â  Â  Â  Â  Â  Â  Â  rem[a++]='0';
+Â  Â  Â  Â  Â  Â  Â  Â  else
+Â  Â  Â  Â  Â  Â  Â  Â  Â  Â  rem[a++]='1';
+Â  Â  Â  Â  Â  Â  }
+Â  Â  Â  Â  Â  Â  rem[a]='\0';
+Â  Â  Â  Â  Â  Â  strcpy(divd,rem);
+Â  Â  Â  Â  }
+Â  Â  Â  Â  else
+Â  Â  Â  Â  {
+Â  Â  Â  Â  Â  Â  strncpy(divd,&divd[1],strlen(divd)-1);
+Â  Â  Â  Â  Â  Â  divd[r-1]='\0';
+Â  Â  Â  Â  }
+Â  Â  Â  Â  int o=strlen(divd);
+Â  Â  Â  Â  divd[o]=agdtw[l+r];
+Â  Â  Â  Â  divd[r]='\0';
+Â  Â  Â  Â  l++;
+Â  Â  }
 
-    strncpy(remd,divd,r-1);
-    remd[r-1]='\0';
+Â  Â  strncpy(remd,divd,r-1);
+Â  Â  remd[r-1]='\0';
 }
 
 void binary(char letter,char bin[])
 {
-    int t,c,i=7;
-    c=(int)letter;
-    while(i>=0)
-    {
-        t=c%2;
-        c=c/2;
-        bin[i--]=t+'0';
-    }
-    bin[8]='\0';
+Â  Â  int t,c,i=7;
+Â  Â  c=(int)letter;
+Â  Â  while(i>=0)
+Â  Â  {
+Â  Â  Â  Â  t=c%2;
+Â  Â  Â  Â  c=c/2;
+Â  Â  Â  Â  bin[i--]=t+'0';
+Â  Â  }
+Â  Â  bin[8]='\0';
 }
 
 char ascii(char bin[])
 {
-    int t=0,c,i=7;
-    while(i>=0)
-    {
-        t=t+pow(2,7-i)*(bin[i]-'0');
-        i--;
-    }
-    return t;
+Â  Â  int t=0,c,i=7;
+Â  Â  while(i>=0)
+Â  Â  {
+Â  Â  Â  Â  t=t+pow(2,7-i)*(bin[i]-'0');
+Â  Â  Â  Â  i--;
+Â  Â  }
+Â  Â  return t;
 }
 
 void main()
-{    
+{Â  Â  
 char dw[126],augdw[1018],div[18],rem[18],cw[1018],rcw[1018],bin[9],rdw[1001],msg[126];
-    printf("Enter a Message to be sent (Max 125 Char)\n");
-    fgets(dw, sizeof(dw), stdin);
-    binary(dw[0],bin);
-    strcpy(augdw,bin);
-    int j,k,e;
-    for(j=1;j<strlen(dw);j++)
-    {
-        binary(dw[j],bin);
-        strcat(augdw,bin);
-    }
-    strcat(augdw,"0000000000000000");
+Â  Â  printf("Enter a Message to be sent (Max 125 Char)\n");
+Â  Â  fgets(dw, sizeof(dw), stdin);
+Â  Â  binary(dw[0],bin);
+Â  Â  strcpy(augdw,bin);
+Â  Â  int j,k,e;
+Â  Â  for(j=1;j<strlen(dw);j++)
+Â  Â  {
+Â  Â  Â  Â  binary(dw[j],bin);
+Â  Â  Â  Â  strcat(augdw,bin);
+Â  Â  }
+Â  Â  strcat(augdw,"0000000000000000");
 
-    printf("\nEnter Divisor (generator) of 17 bits\n");
-    scanf("%s",div);
-    divide(augdw,div,rem);
-    strcpy(cw,augdw);
-    strcpy(&cw[strlen(augdw)-16],rem);
-    strcpy(rcw,cw);
-    printf("\nEnter no. of errors to be introduced during transmission :");
-    scanf("%d",&e);
-    srand(time(0));
-    for(j=0;j<e;j++)
-    {
-        k=rand()%strlen(rcw)-1;
-        if(rcw[k]=='0')
-            rcw[k]='1';
-        else
-            rcw[k]='0';
-        printf("Error Generated at %d th bit %d thcharacter\n",k,(k/8)+1);
-    }
-    divide(rcw,div,rem);
-    if(strcmp(rem,"0000000000000000")!=0)
-        printf("\n\nErroneous Transmission detected!\n");
-    strncpy(rdw,rcw,strlen(rcw)-16);
-    rdw[strlen(rcw)-16]='\0';
-    for(j=0,k=0;j<strlen(rdw);j=j+8)
-    {
-        strncpy(bin,&rdw[j],8);
-        bin[8]='\0';
-        msg[k++]=ascii(bin);
-    }
-    msg[k]='\0';
-    printf("\nRecieved Message = %s\n\n",msg);
+Â  Â  printf("\nEnter Divisor (generator) of 17 bits\n");
+Â  Â  scanf("%s",div);
+Â  Â  divide(augdw,div,rem);
+Â  Â  strcpy(cw,augdw);
+Â  Â  strcpy(&cw[strlen(augdw)-16],rem);
+Â  Â  strcpy(rcw,cw);
+Â  Â  printf("\nEnter no. of errors to be introduced during transmission :");
+Â  Â  scanf("%d",&e);
+Â  Â  srand(time(0));
+Â  Â  for(j=0;j<e;j++)
+Â  Â  {
+Â  Â  Â  Â  k=rand()%strlen(rcw)-1;
+Â  Â  Â  Â  if(rcw[k]=='0')
+Â  Â  Â  Â  Â  Â  rcw[k]='1';
+Â  Â  Â  Â  else
+Â  Â  Â  Â  Â  Â  rcw[k]='0';
+Â  Â  Â  Â  printf("Error Generated at %d th bit %d thcharacter\n",k,(k/8)+1);
+Â  Â  }
+Â  Â  divide(rcw,div,rem);
+Â  Â  if(strcmp(rem,"0000000000000000")!=0)
+Â  Â  Â  Â  printf("\n\nErroneous Transmission detected!\n");
+Â  Â  strncpy(rdw,rcw,strlen(rcw)-16);
+Â  Â  rdw[strlen(rcw)-16]='\0';
+Â  Â  for(j=0,k=0;j<strlen(rdw);j=j+8)
+Â  Â  {
+Â  Â  Â  Â  strncpy(bin,&rdw[j],8);
+Â  Â  Â  Â  bin[8]='\0';
+Â  Â  Â  Â  msg[k++]=ascii(bin);
+Â  Â  }
+Â  Â  msg[k]='\0';
+Â  Â  printf("\nRecieved Message = %s\n\n",msg);
 }
